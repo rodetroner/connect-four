10c1501885fab5e73901a04439dd9fb5fb25945f
rodetroner <rodetroner@gmail.com>
2018-05-05 23:31:28 +0200

    Add 'mechanics' file with Coin class

---
+++ mechanics.py
@@ -0,0 +1,14 @@
+class Coin:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def getColor(self):
+        return self.color
