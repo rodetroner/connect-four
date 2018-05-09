03484cc58fdcc4a007ac814983f66b04b2100005
rodetroner <rodetroner@gmail.com>
2018-05-09 13:20:34 +0200

    Change all 'coins' to 'discs'

--- mechanics.py
+++ mechanics.py
@@ -3,7 +3,7 @@ BOARD_WIDTH = 7
 BOARD_HEIGHT= 5
 
 
-class Coin:
+class Disc:
     def __init__(self, x, y, color):
         self.x = x
         self.y = y
@@ -20,18 +20,18 @@ class Coin:
         return self.color
 
     def __str__(self):
-        return 'Coin: x=' + str(self.x) + ', y=' + str(self.y) + ', color='\
+        return 'Disc: x=' + str(self.x) + ', y=' + str(self.y) + ', color='\
             + self.color
 
     def __repr__(self):
-        return 'Coin(' + str(self.x) + ', ' + str(self.y) + ', '\
+        return 'Disc(' + str(self.x) + ', ' + str(self.y) + ', '\
             + self.color + ')'
 
 
 
 class Game:
     def __init__(self):
-        self.coins = []
+        self.discs = []
         self.current_player = 'red'
         self.game_won = False
         self.game_tied = False
@@ -45,87 +45,87 @@ class Game:
         self.too_high = False
 
     def southConnect(self, new, distance):
-        for old in self.coins:
+        for old in self.discs:
             if new.getColor() == old.getColor():
                 if new.getX() == old.getX() and new.getY()+distance == old.getY():
                     return True
 
     def westConnect(self, new, distance):
-        for old in self.coins:
+        for old in self.discs:
             if new.getColor() == old.getColor():
                 if new.getY() == old.getY() and new.getX()-distance == old.getX():
                     return True
 
     def eastConnect(self, new, distance):
-        for old in self.coins:
+        for old in self.discs:
             if new.getColor() == old.getColor():
                 if new.getY() == old.getY() and new.getX()+distance == old.getX():
                     return True
 
     def northWestConnect(self, new, distance):
-        for old in self.coins:
+        for old in self.discs:
             if new.getColor() == old.getColor():
                 if new.getY()+distance == old.getY() and new.getX()+distance == old.getX():
                     return True
 
     def northEastConnect(self, new, distance):
-        for old in self.coins:
+        for old in self.discs:
             if new.getColor() == old.getColor():
                 if new.getY()+distance == old.getY() and new.getX()-distance == old.getX():
                     return True
 
     def checkForTie(self):
-        if len(self.coins) == BOARD_HEIGHT * BOARD_WIDTH:
+        if len(self.discs) == BOARD_HEIGHT * BOARD_WIDTH:
             return True
         else:
             return False
 
 
     def checkForWin(self):
-        for coin in self.coins:
-            if self.southConnect(coin, 1):
-                if self.southConnect(coin, 2):
-                    if self.southConnect(coin, 3):
+        for disc in self.discs:
+            if self.southConnect(disc, 1):
+                if self.southConnect(disc, 2):
+                    if self.southConnect(disc, 3):
                         return True
 
-            if self.westConnect(coin, 1):
-                if self.westConnect(coin, 2):
-                    if self.westConnect(coin, 3):
+            if self.westConnect(disc, 1):
+                if self.westConnect(disc, 2):
+                    if self.westConnect(disc, 3):
                         return True
 
-            if self.eastConnect(coin, 1):
-                if self.eastConnect(coin, 2):
-                    if self.eastConnect(coin, 3):
+            if self.eastConnect(disc, 1):
+                if self.eastConnect(disc, 2):
+                    if self.eastConnect(disc, 3):
                         return True
 
-            if self.northWestConnect(coin, 1):
-                if self.northWestConnect(coin, 2):
-                    if self.northWestConnect(coin, 3):
+            if self.northWestConnect(disc, 1):
+                if self.northWestConnect(disc, 2):
+                    if self.northWestConnect(disc, 3):
                         return True
 
-            if self.northEastConnect(coin, 1):
-                if self.northEastConnect(coin, 2):
-                    if self.northEastConnect(coin, 3):
+            if self.northEastConnect(disc, 1):
+                if self.northEastConnect(disc, 2):
+                    if self.northEastConnect(disc, 3):
                         return True
 
         return False
 
     def getBottomFreeHoleInColumn(self, column):
         lowest = MIN_HEIGHT
-        for coin in self.coins:
-            if coin.getX() == column:
-                if coin.getY() <= lowest:
-                    lowest = coin.getY()-1
+        for disc in self.discs:
+            if disc.getX() == column:
+                if disc.getY() <= lowest:
+                    lowest = disc.getY()-1
 
         return lowest
 
-    def addCoin(self, column):
+    def addDisc(self, column):
         row = self.getBottomFreeHoleInColumn(column)
         if row == 0:
             self.too_high = True
             return
         else:
-            self.coins.append(Coin(column, row, self.current_player))
+            self.discs.append(Disc(column, row, self.current_player))
         if self.checkForWin():
             self.game_won = True
         if self.checkForTie():
@@ -144,7 +144,7 @@ class Game:
     def tooHigh(self):
         return self.too_high
 
-    def getCoins(self):
-        return self.coins
+    def getDiscs(self):
+        return self.discs
 
 
